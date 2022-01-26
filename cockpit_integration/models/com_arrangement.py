d4ea4c426e3bf3b7864cab2bf2655250eea36ed8
Solene-R <s.roques31@gmail.com>
2022-01-26 10:04:47 +0000

    [ADD] cockpit integration : appel dans la méthode de l'arrangement

--- cockpit_integration/models/com_arrangement.py
+++ cockpit_integration/models/com_arrangement.py
@@ -1,7 +1,11 @@
 from odoo import models, fields, api
-
+import json
+import requests
+from requests.auth import HTTPBasicAuth
+from datetime import date
 
 class com_arrangement(models.Model):
+    ##---------------------------------Fields---------------------------------
     _name = 'cockpit_integration.com_arrangement'
     _description = 'Communication arrangement'
     ##DEV-01-DB-03-01 		 ID 
@@ -10,12 +14,14 @@ class com_arrangement(models.Model):
     operation = fields.Char()
     ##DEV-01-DB-03-03 		 Status 
     status = fields.Selection([('A', 'Active'), ('I', 'Inactive')], string='Status')
+    ## Direction
+    direction = fields.Selection([('IN', 'Inbound'), ('OU', 'Outbound')], string='Direction')
     ## DEV-01-DB-03-04 		 Endpoint (URL) 
     endpoint = fields.Char(string='Endpoint (URL)')
     ## DEV-01-DB-03-05 		 Format 
     formats = fields.Selection([('JS', 'JSON'), ('XM', 'XML'), ('SO', 'SOAP')], string='Format')
     ## DEV-01-DB-03-06 		 Modèle d'identification 
-    formats = fields.Selection([('USR', 'User/password')], string='Identification')
+    identification = fields.Selection([('USR', 'User/password')], string='Identification')
     ## DEV-01-DB-03-07 		 User 
     user = fields.Char(string='User')
     ## DEV-01-DB-03-08 		 Password 
@@ -24,8 +30,70 @@ class com_arrangement(models.Model):
     failContact = fields.Char(string='Fail contact Name')
     ## DEV-01-DB-03-10 		 Contact d'échec (Email) 
     failEmail = fields.Char(string='Fail contact Email')
+    ## originDocType
+    originDocType = fields.Char(string='Origin document type')
+    ## Need for token
+    needToken = fields.Boolean(string='Need to get token first')
+    ## DEV-01-DB-05 		 Com Headers 
+    headers = fields.One2many('cockpit_integration.com_header', 'arrangement', string = 'Header')
     
+    ##---------------------------------Methods---------------------------------
     ## DEV-01-AC-03-01 		L'opération doit être unique
     _sql_constraints = [
         ('uniq_operation', 'unique(operation)', "A communication arrangement already exists with this operation. Operation must be unique!"),
     ]
+    
+    ## Appel d'API
+    def _api_launch_post(self, body, originDocId):
+        for record in self:
+            ##DEV-01-AC-04-01 		URL correspond au endpoint du communication arrangement
+            url = self.endpoint
+            ##DEV-01-AC-04-02 		Le user correspond au user du communication arrangement
+            user = self.user
+            ##DEV-01-AC-04-03 		Le password correspond au password du communication arrangement
+            pwd = self.password
+            ## DEV-01-AC-04-04 		Les headers sont ceux du communication arrangement
+            requHeaders = {}
+            for header in self.headers:
+                requHeaders[header.key] = header.value
+            ## body
+            json_data = body 
+            ## API POST call
+            response = requests.post(url, data=json.dumps(json_data), auth=HTTPBasicAuth(user, pwd), headers=requHeaders)
+
+                        ##DEV-01-AC-01 	3	 Création du tracking 
+            if (response.ok):
+                status = 'Success'
+            else:
+                status = 'Error'
+            self.env['cockpit_integration.track'].create(
+            {
+                'status' : status,
+                'originDocId' : originDocId, ##!!!!!!!!!!!!!!!!!!!
+                'originDocType' : self.originDocType,##!!!!!!!!!!!!!!!!!!!
+                'direction' : self.direction,
+                'operation' : self.operation,
+                'targetUrl' : url,
+                'date' : fields.datetime.now(),
+                'returnCode' : response.status_code,
+                'requestHeader' : requHeaders,
+                'requestBody' : json_data
+            }
+            )
+            
+            ## give back api call response
+            return response
+
+        
+        
+
+    
+class com_header(models.Model):
+    ##---------------------------------Fields---------------------------------
+    _name = 'cockpit_integration.com_header'
+    _description = 'Header'
+    arrangement = fields.Many2one('cockpit_integration.com_arrangement', string = 'Arrangement') 
+    ## DEV-01-DB-05-01 		 Key 
+    key = fields.Char(string = 'Key')
+    ##  DEV-01-DB-05-02 		 Value 
+    value = fields.Char(string = 'Value')
--- cockpit_integration/models/track.py
+++ cockpit_integration/models/track.py
@@ -1,7 +1,10 @@
 from odoo import models, fields, api
-
+import json
+import requests
+from requests.auth import HTTPBasicAuth
 
 class track(models.Model):
+    ##---------------------------------Fields---------------------------------
     _name = 'cockpit_integration.track'
     _description = 'Tracking'
     ##DEV-01-DB-01-01 		 Statut 
@@ -21,20 +24,60 @@ class track(models.Model):
     ## DEV-01-DB-01-08 		 Code de retour 
     returnCode = fields.Char(string='Return Code')
     ## DEV-01-DB-01-09 		 Message de retour 
-    ##returnMsg = fields.Text(string='Return Message')
-    ## DEV-01-DB-01-10 		 Document ID 
-    ##returnDocId = fields.Char(string='Returned Document ID')
-    ## DEV-01-DB-01-11 		 Request ID 
-    ##requestId = fields.Char(string='Request ID')
+    returnReason = fields.Text(string='Return Reason')
+    ## DEV-01-DB-01-10 		 Response text
+    returnText = fields.Char(string='Response')
     ## DEV-01-DB-01-13 		 Request Headers 
     requestHeader = fields.Text(string='Request Headers')
     ## DEV-01-DB-01-14 		 Request body 
     requestBody = fields.Text(string='Request Body')
     ## DEV-01-DB-01-12 		 Tracking Log ID
     log = fields.One2many('cockpit_integration.log', 'track', string = 'Log')
+    resendDate = fields.Datetime(string='Last resend on')
+    
+    ##---------------------------------Methods---------------------------------
+
+    def resend(self):
+        for record in self:
+            ## Retreieve communication arrangement
+            arrangement = self.env['cockpit_integration.com_arrangement'].search([('operation', '=', self.operation)])            
+            ##DEV-01-AC-01-03		Les infos du resend se basent au maximum sur l'accord de communication
+            url = arrangement.endpoint
+            user = arrangement.user
+            pwd = arrangement.password
+            requHeaders = {}
+            for header in arrangement.headers:
+                requHeaders[header.key] = header.value
+            json_data = self.requestBody
+            ## API call
+            response = requests.post(url, data=json.dumps(json_data), auth=HTTPBasicAuth(user, pwd), headers=requHeaders)
+            record.resendDate = fields.datetime.now()
+            ##DEV-01-AC-01 	3	 Création du tracking 
+            if (response.ok):
+                status = 'Success'
+            else:
+                status = 'Error'            
+            self.env['cockpit_integration.track'].create(
+            {
+                'status' : status,
+                'originDocId' : self.originDocId + '_RESEND',
+                'originDocType' : self.originDocType,
+                'direction' : self.direction,
+                'operation' : self.operation,
+                'targetUrl' : url,
+                'date' : fields.datetime.now(),
+                'returnCode' : response.status_code,
+                'returnReason' : response.reason,
+                'returnText' : response.text,
+                'requestHeader' : requHeaders,
+                'requestBody' : json_data
+            }
+            )
+  
 
     
 class log(models.Model):
+    ##---------------------------------Fields---------------------------------
     _name = 'cockpit_integration.log'
     _description = 'Log'
     ## DEV-01-DB-02-01 		 Tracking ID 
